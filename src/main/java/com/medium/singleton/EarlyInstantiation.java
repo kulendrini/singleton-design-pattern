9a75919961b9056ee464b47e3beaeca25b316445
Chamalka Kulendrini Hettiarachchi <32382948+kulendrini@users.noreply.github.com>
2023-09-05 13:41:25 +0530

    Update EarlyInstantiation.java

--- src/main/java/com/medium/singleton/EarlyInstantiation.java
+++ src/main/java/com/medium/singleton/EarlyInstantiation.java
@@ -5,7 +5,7 @@ public class EarlyInstantiation {
 	private static EarlyInstantiation early = new EarlyInstantiation();
 	
 	private EarlyInstantiation() {
-
+		// private Constructor
 	}
 	
 	public static EarlyInstantiation getEarlyInstantiation() {
